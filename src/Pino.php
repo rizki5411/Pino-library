221adfe4019443215e3852c46a90376a99dd3405
Septiyani Putri <113845016+septiynp@users.noreply.github.com>
2022-09-28 11:12:07 +0700

    Update Pino.php

--- src/Pino.php
+++ src/Pino.php
@@ -4,6 +4,13 @@ namespace pino\Library;
 
 use pino\Library\PinoRegex;
 
+/* 
+|-----------------------------------------------------------------------|
+|   This is the Pino Library Core System                                |
+|   Please don't change anything here because it's a static method      |
+|-----------------------------------------------------------------------|
+*/
+
 class Pino{
 
     public static function is($GetTypeData,$GetRequestData) {
