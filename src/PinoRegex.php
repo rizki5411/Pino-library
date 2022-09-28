39dc9d268a37cdb8635f031b97124d2de1ad151f
Septiyani Putri <113845016+septiynp@users.noreply.github.com>
2022-09-28 10:49:14 +0700

    Update PinoRegex.php

--- src/PinoRegex.php
+++ src/PinoRegex.php
@@ -9,21 +9,28 @@ class PinoRegex{
 
     }
 
-    public static function GetRegex($GetRequestData,$GetTypeData) {
+    public static function GetRegex($GetTypeData,$GetRequestData) {
         
-        if($GetTypeData == "string" || $GetTypeData == "String"){
+        if($GetTypeData == "string" || $GetTypeData == "String" || $GetTypeData == "STRING"){
             
             return PinoRegex::String($GetRequestData);
+
+        }else if($GetTypeData == "integer" || $GetTypeData == "Integer" || $GetTypeData == "INTEGER"){
+            
+            return PinoRegex::Integer($GetRequestData);
         }
 
     }
+
     public static function space($GetRequestData){ 
         $value = str_replace(' ', '-', $GetRequestData); 
     }
+
     public static function String($GetRequestData){
              
             return preg_replace('/[^A-Za-z0-9]/', '', $GetRequestData);
     }
+    
     public static function Integer($GetRequestData){
       
             return preg_replace('/[^0-9]/', '', $GetRequestData);
