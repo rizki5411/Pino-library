f6acf95ce36789e9615c5b0c499eafb56221e209
Septiyani Putri <113845016+septiynp@users.noreply.github.com>
2022-09-28 11:11:58 +0700

    Update PinoRegex.php

--- src/PinoRegex.php
+++ src/PinoRegex.php
@@ -2,6 +2,14 @@
 
 namespace pino\Library;
 
+/* 
+|-----------------------------------------------------------------------|
+|   Don't change anything in the dependency, because the changes you    | 
+|   saved can be deleted when you update the dependency.                | 
+|   If you have any other ideas, post them on the Pino-Library github.  |
+|-----------------------------------------------------------------------|
+*/
+
 class PinoRegex{
 
     function __construct()
@@ -13,22 +21,24 @@ class PinoRegex{
         
         if($GetTypeData == "string" || $GetTypeData == "String" || $GetTypeData == "STRING"){
             
+            $GetRequestData = str_replace(' ', '-', $GetRequestData);
             return PinoRegex::String($GetRequestData);
 
         }else if($GetTypeData == "integer" || $GetTypeData == "Integer" || $GetTypeData == "INTEGER"){
             
             return PinoRegex::Integer($GetRequestData);
+        
+        }else{
+
+            return NULL;
         }
 
     }
 
-    public static function space($GetRequestData){ 
-        $value = str_replace(' ', '-', $GetRequestData); 
-    }
-
     public static function String($GetRequestData){
-             
+
             return preg_replace('/[^A-Za-z0-9]/', '', $GetRequestData);
+
     }
     
     public static function Integer($GetRequestData){
